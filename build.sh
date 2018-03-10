dab8e1825ef6526a3c98415f9cd9f468a17ee7e0
etienne <squawkcpp@gmail.com>
2018-03-11 00:59:40 +0100

    update conan build

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ PID=$(sudo docker run -itd -v $(pwd):/repo -v $(pwd)/.build:/.build $IMAGE /bin/
 
 DOCKER_EXEC="sudo docker exec $PID /bin/sh -c"
 
-$DOCKER_EXEC "cd /repo && conan export . conan-cpp/latest && conan install avcpp/0.1.0@conan-cpp/latest --build=avcpp && conan upload avcpp/0.1.0@conan-cpp/latest --all -r=conan-cpp"
+$DOCKER_EXEC "cd /repo && conan export . conan-cpp/latest && conan install avcpp/0.1.0@conan-cpp/latest --build=missing && conan upload avcpp/0.1.0@conan-cpp/latest --all -r=conan-cpp"
 
 sudo docker rm -f $PID
