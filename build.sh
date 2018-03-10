3c80e8c1f595411d0c05ffe7b08000df21e0100c
etienne <squawkcpp@gmail.com>
2018-03-11 00:56:19 +0100

    update conan build

--- build.sh
+++ build.sh
@@ -8,13 +8,6 @@ PID=$(sudo docker run -itd -v $(pwd):/repo -v $(pwd)/.build:/.build $IMAGE /bin/
 
 DOCKER_EXEC="sudo docker exec $PID /bin/sh -c"
 
-$DOCKER_EXEC "cd .build && conan install /repo --build=missing"
-$DOCKER_EXEC "cmake -H/repo -B/.build -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja -DAVCPP_TAG_VERSION=$TAG"
-$DOCKER_EXEC "cmake --build /.build"
-#$DOCKER_EXEC "cmake --build /.build --target package"
-$DOCKER_EXEC "conan user -p $BINTRAY_KEY -r conan-cpp squawkcpp"
-$DOCKER_EXEC "cd /repo && conan export . conan-cpp/latest && conan install avcpp/0.0.1@conan-cpp/latest --build=avcpp && conan upload avcpp/0.0.1@conan-cpp/latest --all -r=conan-cpp"
-
-#sudo docker build -f docker/Dockerfile --build-arg AVCPP_TAG_VERSION=$TAG -t avcpp .
+$DOCKER_EXEC "cd /repo && conan export . conan-cpp/latest && conan install avcpp/0.1.0@conan-cpp/latest --build=avcpp && conan upload avcpp/0.1.0@conan-cpp/latest --all -r=conan-cpp"
 
 sudo docker rm -f $PID
--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,8 @@
 from conans import ConanFile, CMake, tools
 import zipfile
 
-class AvCppConan(ConanFile):
-    name = "AvCpp"
+class AvcppConan(ConanFile):
+    name = "avcpp"
     version = '0.1.0'
     url = 'https://github.com/cppwrappers/LibAvCpp'
     license = 'LGPL'
@@ -12,17 +12,17 @@ class AvCppConan(ConanFile):
     requires = "ffmpeg/3.4.1@conan-cpp/latest", "bzip2/1.0.6@conan/stable", "lzma/5.2.3@bincrafters/stable", "zlib/1.2.11@conan/stable", "gtest/1.8.0@bincrafters/stable"
     options = { "shared": [True, False], "build_tests":  [True, False], "build_samples": [True, False]}
     default_options = "shared=False", "*:shared=False", "build_tests=False", "build_samples=False"
-    exports_sources = "."
+    exports_sources = "*", "av/"
 
     def build(self):
         cmake = CMake(self)
         cmake.configure(defs={
                 "CMAKE_INSTALL_PREFIX": self.package_folder
-            }, source_dir="LibAvCpp-master")
+            }, source_dir=".")
         cmake.build(target="install")
 
     def package(self):
-        self.copy('*.h', dst='include', src='LibAvCpp-master/')
+        self.copy('*.h', dst='include', src='.')
         if self.options.shared:
             if self.settings.os == "Macos":
                 self.copy(pattern="*.dylib", dst="lib", keep_path=False)
