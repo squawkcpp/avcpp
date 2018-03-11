d6ed6798ad61ab1a9ed9b4cb1693bfe5cafadd3b
etienne <squawkcpp@gmail.com>
2018-03-11 01:08:31 +0100

    update conan build

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 set -e
 
 IMAGE=spielhuus/toolchain
-TAG=`if [ -z "$1" ]; then echo "master"; else echo "$1" ; fi`
+TAG=`if [ -z "$2" ]; then echo "master"; else echo "$2" ; fi`
 PID=$(sudo docker run -itd -v $(pwd):/repo -v $(pwd)/.build:/.build $IMAGE /bin/sh)
 
 DOCKER_EXEC="sudo docker exec $PID /bin/sh -c"
-
+$DOCKER_EXEC "conan user -p $1 -r conan-cpp squawkcpp"
 $DOCKER_EXEC "cd /repo && conan export . conan-cpp/latest && conan install avcpp/0.1.0@conan-cpp/latest --build=missing && conan upload avcpp/0.1.0@conan-cpp/latest --all -r=conan-cpp"
 
 sudo docker rm -f $PID
